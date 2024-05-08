bbf70de630fc084584d7262e118756a0491a0ca8
Antoine-NVE <antoinenavette@gmail.com>
2024-05-08 19:12:13 +0200

    Episode 10 : Enregistrer une composante (Bootstrapping) et comprendre les erreurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    // template: '<h1>{{ title }}</h1>',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Tuto_Angular';
+    title = 'Tuto_Angular';
 }
