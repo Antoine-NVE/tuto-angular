e2a698b2afd1f425c33d3414f12711f4f0792a30
Antoine-NVE <antoinenavette@gmail.com>
2024-05-11 09:45:10 +0200

    Episode 20 : Créer un Pipe personnalisé

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 import { IHotel } from './hotel';
+import { ReplaceComma } from '../shared/pipes/replace-comma.pipe';
 
 registerLocaleData(localeFr, 'fr');
 
 @Component({
     selector: 'app-hotel-list',
     standalone: true,
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, ReplaceComma],
     templateUrl: './hotel-list.component.html',
     styleUrl: './hotel-list.component.css',
 })
@@ -50,13 +51,39 @@ export class HotelListComponent implements OnInit {
 
     public showBadge: boolean = false;
 
-    public hotelFilter: string = 'mot';
+    private _hotelFilter: string = 'mot';
+
+    public filteredHotels: IHotel[] = [];
 
     public ngOnInit(): void {
-        console.log('test');
+        this.filteredHotels = this.hotels;
+        this.hotelFilter = '';
     }
 
     public toggleIsNewBadge(): void {
         this.showBadge = !this.showBadge;
+    }
+
+    public get hotelFilter(): string {
+        return this._hotelFilter;
+    }
+
+    public set hotelFilter(filter: string) {
+        this._hotelFilter = filter;
+
+        this.filteredHotels = this.hotelFilter
+            ? this.filterHotels(this.hotelFilter)
+            : this.hotels;
+    }
+
+    private filterHotels(criteria: string): IHotel[] {
+        criteria = criteria.toLocaleLowerCase();
+
+        const res = this.hotels.filter(
+            (hotel: IHotel) =>
+                hotel.hotelName.toLocaleLowerCase().indexOf(criteria) !== -1
+        );
+
+        return res;
     }
 }
---
+++ src/app/shared/pipes/replace-comma.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'replaceComma',
+    standalone: true
+})
+export class ReplaceComma implements PipeTransform {
+    transform(value: string|null): string {
+        if (!!value) {
+            return value.replace(',', '.');
+        } else {
+            return '';
+        }
+    }
+}
