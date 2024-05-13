ca90c9f2a8668d91b8875474d3097d5b5a2dde9f
Antoine-NVE <antoinenavette@gmail.com>
2024-05-13 11:20:06 +0200

    Episode 24 : Comprendre les services et l'injection des dépendances

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -6,6 +6,7 @@ import localeFr from '@angular/common/locales/fr';
 import { IHotel } from './hotel';
 import { ReplaceComma } from '../shared/pipes/replace-comma.pipe';
 import { StarRatingComponent } from '../shared/components/star-rating/star-rating.component';
+import { HotelListService } from './hotel-list.service';
 
 registerLocaleData(localeFr, 'fr');
 
@@ -19,40 +20,7 @@ registerLocaleData(localeFr, 'fr');
 export class HotelListComponent implements OnInit {
     public title = "Liste d'hôtels";
 
-    public hotels: IHotel[] = [
-        {
-            hotelId: 1,
-            hotelName: 'Buea sweet life',
-            description: 'Belle vue au bord de la mer',
-            price: 230.5,
-            imageUrl: 'assets/img/hotel-room.jpg',
-            rating: 3.5,
-        },
-        {
-            hotelId: 2,
-            hotelName: 'Marakech',
-            description: 'Profitez de la vue sur les montagnes',
-            price: 145.5,
-            imageUrl: 'assets/img/the-interior.jpg',
-            rating: 5,
-        },
-        {
-            hotelId: 3,
-            hotelName: 'Buea sweet life',
-            description: 'Belle vue au bord de la mer',
-            price: 120.12,
-            imageUrl: 'assets/img/indoors.jpg',
-            rating: 4,
-        },
-        {
-            hotelId: 4,
-            hotelName: 'Cape town city',
-            description: 'Magnifique cadre pour votre séjour',
-            price: 135.12,
-            imageUrl: 'assets/img/window.jpg',
-            rating: 2.5,
-        },
-    ];
+    public hotels: IHotel[] = [];
 
     public showBadge: boolean = false;
 
@@ -62,7 +30,10 @@ export class HotelListComponent implements OnInit {
 
     public receivedRating: string = '';
 
+    constructor(private hotelListService: HotelListService) {}
+
     public ngOnInit(): void {
+        this.hotels = this.hotelListService.getHotels();
         this.filteredHotels = this.hotels;
         this.hotelFilter = '';
     }
