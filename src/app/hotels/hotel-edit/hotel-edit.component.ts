f23bfc88e063e2893d5818f84a186a25771f4f3f
Antoine-NVE <antoinenavette@gmail.com>
2024-05-17 10:33:51 +0200

    Episode 36 : Connecter le template au model avec le FormBuilder

--- src/app/hotels/hotel-edit/hotel-edit.component.ts
+++ src/app/hotels/hotel-edit/hotel-edit.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-hotel-edit',
-  standalone: true,
-  imports: [],
-  templateUrl: './hotel-edit.component.html',
-  styleUrl: './hotel-edit.component.css'
+    selector: 'app-hotel-edit',
+    standalone: true,
+    imports: [FormsModule, ReactiveFormsModule, CommonModule, RouterLink],
+    templateUrl: './hotel-edit.component.html',
+    styleUrl: './hotel-edit.component.css',
 })
 export class HotelEditComponent {
+    public hotelForm: FormGroup = this.fb.group({
+        hotelName: ['', Validators.required],
+        hotelPrice: ['', Validators.required],
+        starRating: [''],
+        description: [''],
+    });
 
+    constructor(private fb: FormBuilder) {}
+
+    public saveHotel(): void {
+        console.log(this.hotelForm.value);
+    }
 }
